15cb0754ce2cb54fd8b7a49e0587418671392e2d
ilko.garcia <ilko.garcia@gmail.com>
2023-02-03 19:01:20 +0100

    I update the prototype with the initial page completed. Includes a popo-up to show the rules of the game.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showRules() {
+    document.getElementById('rules-container').style.visibility = 'visible';
+    return;
+}
+
+function hideRules() {
+    document.getElementById('rules-container').style.visibility = 'hidden';
+    return;
+}
