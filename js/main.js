07a56637be08b0c3b098edf3b7d55f07d4f4204c
ilko.garcia <ilko.garcia@gmail.com>
2023-02-07 09:19:23 +0100

    Add comments in the main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
+/*
+* Shows hidden elements in the html
+* @param {string} element Id
+*/
 const showElement = (elementId) => {
     document.getElementById(elementId).style.visibility = 'visible';
 }
 
+/*
+* Hide elements visible in the html
+* @param {string} element Id
+*/
 const hideElement = (elementId) => {
     document.getElementById(elementId).style.visibility = 'hidden';
 }
 
+/*
+* Check if input text is empty
+* @param {string} element Id
+*/
 const validateInputText = (elementId) => {
     let a = document.getElementById(elementId).value;
     if ( a == null || a == "") {
