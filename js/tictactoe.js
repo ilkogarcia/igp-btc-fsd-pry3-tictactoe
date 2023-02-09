158b1820b2e29e05f36eac9929cb6a7cdad3d63c
ilko.garcia <ilko.garcia@gmail.com>
2023-02-09 17:22:45 +0100

    Now as game tiles we use images to give it a better appearance

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,26 +2,35 @@
  * Contains methods for handling user events and game states on game page.
  */
 
-// Setting initial parameters for player 1
-let player1Name = sessionStorage.getItem("player1-name");   // Player 1. Get name from sessionStorage
-let player1Score = 0;                                       // Player 1. Initial score
-let player1Tokens = 3;                                      // Player 1. Max tokens
+let player1Name, player1Score ,player1Tokens;       // All player 1 variables.
+let player2Name, player2Score ,player2Tokens;       // All player 2 variables.
+let winner;                                         // Store the winner of a game.
+let gameTurn;                                       // True means is "X" turn otherwise is "O" turn.
+let currentBoard;                                   // We store in an array the state of the board.
 
-// Setting initial parameters for player 2
-let player2Name = sessionStorage.getItem("player2-name");   // Player 2. Get name from sessionStorage
-let player2Score = 0;                                       // Player 2. Initial score
-let player2Tokens = 3;                                      // Player 2. Max tokens
+const initializeGame = () => {
+    // Setup initial parameters for player 1
+    player1Name = sessionStorage.getItem("player1-name");   // Player 1. Get name from sessionStorage.
+    player1Score = 0;                                       // Player 1. Initial score.
+    player1Tokens = 3;                                      // Player 1. Max tokens.
 
-// Initializing game scoreboard
-document.getElementById("player1-name").innerHTML = player1Name;
-document.getElementById("player1-score").innerHTML = player1Score;
-document.getElementById("player2-name").innerHTML = player2Name;
-document.getElementById("player2-score").innerHTML = player2Score;
+    // Setup initial parameters for player 2
+    player2Name = sessionStorage.getItem("player2-name");   // Player 2. Get name from sessionStorage.
+    player2Score = 0;                                       // Player 2. Initial score.
+    player2Tokens = 3;                                      // Player 2. Max tokens.
 
-// Setting general parameters
-let gameTurn = true;        // When gameTurn is true means is "X" turn otherwise is "O" turn.
-let currentBoard = ["", "", "", "", "", "", "", "", ""];
-let winner;
+    // Setup scoreboard initial status
+    document.getElementById("player1-name").innerHTML = player1Name.substring(0, 6);
+    document.getElementById("player1-score").innerHTML = player1Score;
+    document.getElementById("player2-name").innerHTML = player2Name.substring(0, 6);
+    document.getElementById("player2-score").innerHTML = player2Score; 
+
+    // Setup general parameters
+    gameTurn = true;                                        // The player with the X is always the first to move.
+    currentBoard = ["", "", "", "", "", "", "", "", ""];    // The game board is empty.
+
+}
+initializeGame();
 
 const checkBoardStatus = () => {
     // Checking Horizontal Wins
@@ -149,37 +158,42 @@ let gameBoard = Array.from(document.getElementsByClassName("Gameboard-cell"));
 gameBoard.map (
     (gameCell) => {
         gameCell.addEventListener('click', () => {
-
                 if ((gameCell.innerHTML == "") && (player1Tokens > 0 || player2Tokens > 0)) {
-                    gameCell.innerHTML = (gameTurn) ? "X" : "O";        // Draw X or O according players turn
-                    (gameTurn) ? player1Tokens-- : player2Tokens--;     // Decrease a turn to the corresponding player
-                    currentBoard[gameCell.id] = (gameTurn) ? "X" : "O"; // Update on memory gameboard status
-                    winner = checkBoardStatus();
-                    if (winner) {                                       // Check if we have a winner
+                    gameCell.innerHTML = (gameTurn) ? "X" : "O";                // Draw X or O according turn
+                    gameCell.classList.add(
+                        (gameTurn) ? "Gameboard-cell-x" : "Gameboard-cell-y"    // Draw background X or O according turn
+                    );
+                    (gameTurn) ? player1Tokens-- : player2Tokens--;             // Decrease a turn to the corresponding player
+                    currentBoard[gameCell.id] = (gameTurn) ? "X" : "O";         // Update on memory gameboard status
+                    winner = checkBoardStatus();                                // Check if we have a winner
+                    if (winner) {
                         paintWinningCombo(winner);
-                        // Executes only once after the delay whereas setInterval keeps on.
-                        let delayInMilliseconds = 2000;     // 2 second
                         setTimeout(function() {
-                            //your code to be executed after 1 second
-                            showWinner(currentBoard[gameCell.id]);
-                        }, delayInMilliseconds);
+                            showWinner(currentBoard[gameCell.id]);              // Show winner with 1 sec delay
+                        }, 1000);
                         updateScore(currentBoard[gameCell.id]);
                     }
-                    gameTurn = !gameTurn;                               // Change player turn
+                    gameTurn = !gameTurn;                                       // Change player turn
                 } else if ((gameCell.innerHTML != "") && (player1Tokens > 0 || player1Tokens > 0)) {
                     showMessage("In this phase of the game clicking on a filled cell is not allowed");
-                } else if ((gameCell.innerHTML == "" || gameCell.innerHTML == null) && (player1Score == 0 || player2Score == 0)) {
+                } else if ((gameCell.innerHTML == "") && (player1Score == 0 || player2Score == 0)) {
                     showMessage("You have no tokens available. You must move some of the pieces that you have on the board.");
                 } else if ((gameCell.innerHTML == "X") && gameTurn && (player1Tokens == 0)) {
-                    gameCell.innerHTML = "";                            // Erase cell content
-                    currentBoard[gameCell.id] = "";                     // Update on memory gameboard status
-                    player1Tokens++                                     // Add a turn to player 1
+                    gameCell.innerHTML = "";                                    // Erase cell content
+                    currentBoard[gameCell.id] = "";                             // Update on memory gameboard status
+                    gameCell.classList.remove(                                  // Update on physical gameboard status
+                        (gameTurn) ? "Gameboard-cell-x" : "Gameboard-cell-y"
+                        );
+                    player1Tokens++                                             // Add a turn to player 1
                 } else if ((gameCell.innerHTML == "X") && !gameTurn && (player1Tokens == 0)) {
                     showMessage("You cannot move your opponent's pieces.");
                 } else if ((gameCell.innerHTML == "O") && !gameTurn && (player2Tokens == 0)) {
-                    gameCell.innerHTML = "";                            // Erase cell content
-                    currentBoard[gameCell.id] = "";                     // Update on memory gameboard status
-                    player2Tokens++                                     // Add a turn to player 2
+                    gameCell.innerHTML = "";                                    // Erase cell content
+                    currentBoard[gameCell.id] = "";                             // Update on memory gameboard status
+                    gameCell.classList.remove(                                  // Update on physical gameboard status
+                    (gameTurn) ? "Gameboard-cell-x" : "Gameboard-cell-y"
+                    );
+                    player2Tokens++                                             // Add a turn to player 2
                 } else if ((gameCell.innerHTML == "O") && gameTurn && (player2Tokens == 0)) {
                     showMessage("You cannot move your opponent's pieces.");
                 }
@@ -204,7 +218,14 @@ newGameButton.addEventListener('click', () => {
 
 // Reset the game board for a new game
 const resetGame = () => {
-    gameBoard.map ((gameCell) => (gameCell.innerHTML = "", gameCell.classList.remove("Gameboard-cell-winner")));
+    gameBoard.map (
+        (gameCell) => (
+            gameCell.innerHTML = "", 
+            gameCell.classList.remove("Gameboard-cell-winner"),
+            gameCell.classList.remove("Gameboard-cell-x"),
+            gameCell.classList.remove("Gameboard-cell-y")
+            )
+        );
     player1Tokens = 3;      // Player 1. Max tokens
     player2Tokens = 3;      // Player 2. Max tokens
     gameTurn = true;        // When gameTurn is true means is "X" turn otherwise is "O" turn.
